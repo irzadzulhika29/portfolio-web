77b11c06f4299ddc921268b402bc1a8b628f4baf
irzadzulhika29 <irjajulhika@gmail.com>
2026-06-15 09:07:47 +0700

    fix: remove favicon.ico and add new icon.png for updated branding

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["172.16.2.201"],
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
